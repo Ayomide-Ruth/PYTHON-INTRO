3eb8c50e66f9fb428090958386e0e952077763ac
Ayomide-Ruth <ogungbesanayomide@gmail.com>
2026-08-14 12:31:03 +0100

    update

---
+++ quiz_data.py
@@ -0,0 +1,14 @@
+quiz_questions = [
+    ("Where was Jesus born?", "Bethelem"),
+    ("Who was king when Jesus was born?", "King Herod"),
+    ("What are the names of Jesus' parents?", "Mary and Joseph"),
+    ("How many disciples did Jesus have?", 12),
+    ("Who betrayed Jesus?", "Judas Ischariot")
+    ]
+
+
+def get_questions():
+    return quiz_questions
+
+print(quiz_questions)
+
---
+++ topic_12.py
@@ -0,0 +1,36 @@
+from quiz_data import get_questions
+import random, datetime
+
+quiz_questions = get_questions()
+
+print(quiz_questions)
+
+print("=" * 40)
+random.shuffle(quiz_questions)
+print(quiz_questions)
+
+questions_only = []
+answers_only = []
+user_response = []
+
+for question in quiz_questions:
+    print(question[0])
+    questions_only.append(question[0])
+    answers_only.append(question[1])
+
+print(questions_only)
+print(answers_only)
+
+def ask(question):
+    response = input(f"{question}:")
+    return response
+    print(response)  
+
+
+while True:
+    for quiz in questions_only:
+        answer = ask(quiz)
+        user_response.append(answer)
+        #print(answer)
+
+    break
